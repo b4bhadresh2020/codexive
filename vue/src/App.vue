230f107f8a375cccda6be27f5443d57a0d1be49d
brijesh <brijesh@bizspice.com>
2021-02-20 17:47:23 +0530

    login design and api set

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <Login/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import Login from './components/Login';
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,15 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import vueRouter from 'vue-router';
+import axios from 'axios'
+Vue.use(vueRouter);
+
+axios.defaults.baseURL = "http://decd265b3d6d.ngrok.io/api/"
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
